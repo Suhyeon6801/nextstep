354b5651899ed8ff0591fe572007d14f6f145b1e
Suhyeon6801 <k0s3y0@naver.com>
2021-01-24 17:17:03 +0900

    [NextStep_board] feat : 요구사항5 - 로그인하기
    # 로그인 성공 시 index.html 페이지로 이동, 실패하면 logined_fail.html페이지로 이동
    # 회원가입한 사용자가 로그인 이용 가능
    # 로그인 성공시 로그인 상태 유지
    # 로그인이 성공하면 요청의 Cookie 헤더 값이 logined=true, 실패하면 logined=false로 전달

--- web-application-server/src/main/java/webserver/RequestHandler.java
+++ web-application-server/src/main/java/webserver/RequestHandler.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import db.DataBase;
 import model.User;
 import util.HttpRequestUtils;
 import util.IOUtils;
@@ -56,20 +57,46 @@ public class RequestHandler extends Thread {
 				Map<String, String> params = HttpRequestUtils.parseQueryString(requestbody);
 				User user = new User(params.get("userId"), params.get("password"), params.get("name"),
 					params.get("email"));
-				log.debug("User : {}", user);
+				DataBase.addUser(user);
+			} else if ("/user/login".equals(url)) {
+				String body = IOUtils.readData(br, contentLength);
+				Map<String, String> params = HttpRequestUtils.parseQueryString(body);
+				User user = DataBase.findUserById(params.get("userId"));
+				if (user == null) {
+					responseResource(out, "/user/login_failed.html");
+					return;
+				}
 
-				DataOutputStream dos = new DataOutputStream(out);
-				response302Header(dos, "/index.html");
+				if (user.getPassword().equals(params.get("password"))) {
+					DataOutputStream dos = new DataOutputStream(out);
+					response302LoginSuccessHeader(dos);
+				} else {
+					responseResource(out, "/user/login_failed.html");
+				}
+			} else {
+				responseResource(out, url);
 			}
-
-			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-			response200Header(dos, body.length);
-			responseBody(dos, body);
-
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
+	}
+
+	private void response302LoginSuccessHeader(DataOutputStream dos) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+			dos.writeBytes("Set-cookie : logined=true \r\n");
+			dos.writeBytes("Location : /index.html \r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	private void responseResource(OutputStream out, String url) throws IOException {
+		DataOutputStream dos = new DataOutputStream(out);
+		byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+		response200Header(dos, body.length);
+		responseBody(dos, body);
 	}
 
 	/**
