93632425c403b4ead988ec28e8b4cd3428130723
Suhyeon6801 <k0s3y0@naver.com>
2021-02-02 14:36:36 +0900

    [NextStep_board] refact : request line 처리하는 메소드 클래스로 분리

--- web-application-server/src/main/java/webserver/HttpRequest.java
+++ web-application-server/src/main/java/webserver/HttpRequest.java
@@ -24,67 +24,49 @@ import util.IOUtils;
 public class HttpRequest {
 	private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
 
-	private String method;
-	private String path;
 	private Map<String, String> headers = new HashMap<String, String>();
 	private Map<String, String> params = new HashMap<String, String>();
+	private RequestLine requestLine;
 
 	public HttpRequest(InputStream in) {
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 			String line = br.readLine();
-			if(line==null) {
+			if (line == null) {
 				return;
 			}
-			
-			processRequestLine(line);
+
+			requestLine = new RequestLine(line);
 			while (!line.equals("")) {
 				log.debug("header : {}", line);
 				String[] tokens = line.split(":");
 				headers.put(tokens[0].trim(), tokens[1].trim());
 				line = br.readLine();
-				
-				if("POST".equals(method)) {
+
+				if ("POST".equals(getMethod())) {
 					String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
-					params=HttpRequestUtils.parseQueryString(body);
+					params = HttpRequestUtils.parseQueryString(body);
+				} else {
+					params = requestLine.getParams();
 				}
 			}
 		} catch (IOException io) {
 
 		}
 	}
-	
-	private void processRequestLine(String requestLine) {
-		log.debug("request line : {}", requestLine);
-		String tokens[] = requestLine.split(" ");
-		method=tokens[0];
-		
-		if("POST".equals(method)) {
-			path=tokens[1];
-			return;
-		}
-		
-		int index=tokens[1].indexOf("?");
-		if(index==-1) {
-			path=tokens[1];
-		}else {
-			path=tokens[1].substring(0, index);
-			params=HttpRequestUtils.parseQueryString(tokens[1].substring(index+1));
-		}
-	}
 
 	public String getMethod() {
-		return method;
+		return requestLine.getMethod();
 	}
-	
+
 	public String getPath() {
-		return path;
+		return requestLine.getPath();
 	}
-	
+
 	public String getHeader(String name) {
 		return headers.get(name);
 	}
-	
+
 	public String getParameter(String name) {
 		return params.get(name);
 	}
---
+++ web-application-server/src/main/java/webserver/RequestLine.java
@@ -0,0 +1,60 @@
+package webserver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import util.HttpRequestUtils;
+
+/**
+ * 요청라인을 처리하는 클래스
+ * <p>
+ * HttpRequest의 processRequsetLine 함수를 class로 분리
+ * 
+ * @author Suhyeon Kim
+ *
+ */
+public class RequestLine {
+	private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+
+	private String method;
+	private String path;
+	private Map<String, String> params = new HashMap<String, String>();
+
+	public RequestLine(String requestLine) {
+		log.debug("request line : {}", requestLine);
+		String tokens[] = requestLine.split(" ");
+
+		if (tokens.length != 3) {
+			throw new IllegalArgumentException(requestLine + "이 형식에 맞지 않습니다.");
+		}
+
+		method = tokens[0];
+		if ("POST".equals(method)) {
+			path = tokens[1];
+			return;
+		}
+
+		int index = tokens[1].indexOf("?");
+		if (index == -1) {
+			path = tokens[1];
+		} else {
+			path = tokens[1].substring(0, index);
+			params = HttpRequestUtils.parseQueryString(tokens[1].substring(index + 1));
+		}
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public Map<String, String> getParams() {
+		return params;
+	}
+}
