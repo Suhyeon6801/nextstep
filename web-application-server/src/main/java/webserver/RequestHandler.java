012859c5fbe3b5134b1f0bcbb92da1eb62c775b6
Suhyeon6801 <k0s3y0@naver.com>
2021-02-03 13:51:19 +0900

    [NextStep_board_다형성실습] refact : RequestMapping 작성, RequestHandler에서 요청 URL에 대한 처리 Controller로 위임
    # 정보수정, 로그아웃 등 새로운 기능이 추가된다면, run() 메소드에서 수정하지 않아도 됨
    # 새로운 기능이 추가된다면 Controller 인터페이스를 구현하는 클래스를 추가 후 RequestMapping에서 연결해주면 됨
    # 각 클래스 간에는 어떤 영향도 미치지 않고 새로운 기능을 추가하는 것이 가능
    # 변경사항이 발생하면 다른 클래스에 영향 미치지 않고 해당 Controller의 service메소드만 수정하면 됨.

--- web-application-server/src/main/java/webserver/RequestHandler.java
+++ web-application-server/src/main/java/webserver/RequestHandler.java
@@ -4,17 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Collection;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import db.DataBase;
+import controller.Controller;
 import http.HttpRequest;
 import http.HttpResponse;
-import model.User;
-import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -32,86 +28,24 @@ public class RequestHandler extends Thread {
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			HttpRequest request = new HttpRequest(in);
 			HttpResponse response = new HttpResponse(out);
-			String path = getDefaultPath(request.getPath());
 
-			if ("/user/create".equals(path)) {
-				createUser(request, response);
-			} else if ("/user/login".equals(path)) {
-				login(request, response);
-			} else if ("/user/list".equals(path)) {
-				listUser(request, response);
-			} else {
+			Controller controller = RequestMapping.getController(request.getPath());
+			if (controller == null) {
+				String path = getDefaultPath(request.getPath());
 				response.forward(path);
+			} else {
+				controller.service(request, response);
 			}
+
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
 	}
 
-	private void createUser(HttpRequest request, HttpResponse response) {
-		User user = new User(request.getParameter("userId"),
-			request.getParameter("password"),
-			request.getParameter("name"),
-			request.getParameter("email"));
-		log.debug("user : {}", user);
-		DataBase.addUser(user);
-		response.sendRedirect("/index.html");
-	}
-
-	private void login(HttpRequest request, HttpResponse response) {
-		User user = DataBase.findUserById(request.getParameter("userId"));
-		if (user != null) {
-			if (user.login(request.getParameter("userId"))) {
-				response.addHeader("Set-Cookie", "logined=true; Path=/");
-				response.sendRedirect("/index.html");
-			} else {
-				response.sendRedirect("/user/login_failed.html");
-			}
-		} else {
-			response.sendRedirect("/user/login_failed.html");
-		}
-	}
-
-	private void listUser(HttpRequest request, HttpResponse response) {
-		if (!isLogin(request.getHeader("Cookie"))) {
-			response.sendRedirect("/user/login.html");
-			return;
-		}
-
-		Collection<User> users = DataBase.findAll();
-		StringBuilder sb = new StringBuilder();
-		sb.append("<table border='1'>");
-		for (User user : users) {
-			sb.append("<tr>");
-			sb.append("<td>" + user.getUserId() + "</td>");
-			sb.append("<td>" + user.getName() + "</td>");
-			sb.append("<td>" + user.getEmail() + "</td>");
-			sb.append("</tr>");
-		}
-		sb.append("</table>");
-
-		response.forwardBody(sb.toString());
-	}
-
 	private String getDefaultPath(String path) {
-		if (path.contentEquals("/")) {
+		if (path.equals("/")) {
 			return "/index.html";
 		}
 		return path;
 	}
-
-	/**
-	 * 로그인 여부 확인하는 함수
-	 * @param cookieValue
-	 * @return boolean
-	 */
-	private boolean isLogin(String cookieValue) {
-		Map<String, String> cookies = HttpRequestUtils.parseCookies(cookieValue);
-		String value = cookies.get("logined");
-		if (value == null) {
-			return false;
-		}
-		return Boolean.parseBoolean(value);
-	}
-
 }
